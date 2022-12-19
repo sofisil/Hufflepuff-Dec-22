f16b5dd7720f958ead9049a82d902453862d9b5c
JenJen1103 <jenniferrios1103@gmail.com>
2022-12-19 16:48:54 -0300

    Form html

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ class Usuarios (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(80), nullable = False)
     password = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(80), nullable = False)
+    ciudad = db.Column(db.String(80), nullable = False)
+    edad = db.Column(db.Integer, nullable = False)
+
 
     def __init__(self, usuario, password):
         self.usuario = usuario
